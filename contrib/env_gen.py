b899f0aca0b74cdce665b6b0df01d0c2d2a9ecb7
rg3915 <regis42santos@gmail.com>
2022-09-07 20:53:26 -0300

    Define novas variáveis de ambiente como exemplo.

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -18,10 +18,11 @@ ALLOWED_HOSTS=127.0.0.1,.localhost,0.0.0.0
 DEV=True
 
 #DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
-#POSTGRES_DB=
-#POSTGRES_USER=
-#POSTGRES_PASSWORD=%s
-#DB_HOST=localhost
+POSTGRES_DB=db
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+DB_HOST=db  # para Docker ou localhost
+DB_PORT=5432
 
 #DEFAULT_FROM_EMAIL=
 #EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
@@ -30,7 +31,7 @@ DEV=True
 #EMAIL_HOST_USER=
 #EMAIL_HOST_PASSWORD=
 #EMAIL_USE_TLS=True
-""".strip() % (secret_key, password)
+""".strip() % secret_key
 
 # Writing our configuration file to '.env'
 with open('.env', 'w') as configfile:
