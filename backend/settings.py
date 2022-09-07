fcca32f4d6cd0b9b911abe11a06053883e627f62
rg3915 <regis42santos@gmail.com>
2022-09-07 00:53:06 -0300

    fix static on settings

--- backend/settings.py
+++ backend/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 # Default primary key field type
