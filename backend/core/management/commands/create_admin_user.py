730bfc07320c53a582a9b8f3405a12be81579277
rg3915 <regis42santos@gmail.com>
2022-09-07 02:43:01 -0300

    Cria novo comando create_admin_user.py

---
+++ backend/core/management/commands/create_admin_user.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+'''
+Usage: python manage.py create_admin_user -p sua-senha
+'''
+
+
+class Command(BaseCommand):
+    help = "Cria um super usuário admin."
+
+    def add_arguments(self, parser):
+        parser.add_argument('-p', dest='password', help='Digite a senha.')
+
+    def handle(self, *args, **options):
+        password = options.get('password')
+
+        if not password:
+            print('Digite -p e informe a senha.')
+            return
+
+        if User.objects.filter(username='admin').exists():
+            print('admin já existe!')
+        else:
+            User.objects.create_superuser('admin', 'admin@email.com', password)
