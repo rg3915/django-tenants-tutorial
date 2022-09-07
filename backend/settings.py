9d7d1cfbcf8fe31efe84166bec98a132ac634ee5
rg3915 <regis42santos@gmail.com>
2022-09-07 01:18:43 -0300

    remove config decouple do settings temporariamente

--- backend/settings.py
+++ backend/settings.py
@@ -10,14 +10,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '7^My?4eh&js%NEHpqSD3cgTl0io0aWXn(*U@kQ1K6u)Y$#wbJv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = []
 
-DEV = config('DEV', default=False, cast=bool)
+# DEV = config('DEV', default=False, cast=bool)
+DEV = False
 
 # Application definition
 
